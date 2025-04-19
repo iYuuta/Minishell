ed307c0fa946a9c9134ec8d887d80f8e09db1fcd
Youssef Ayedder <yoayedde@e2r5p5.1337.ma>
2025-04-19 17:55:39 +0100

    finished export and cd

--- test_execv.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main() {
-    char *args[] = {"/bin/ls", "-a" "", NULL};
-    execv(args[0], args);
-    perror("execv"); // execv only returns on error
-    return 1;
-}
